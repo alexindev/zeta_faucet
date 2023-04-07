4cc4d896bae35b8ce987a4ec7fba446ace2bdcf6
alex <55256281+alexindev@users.noreply.github.com>
2023-04-08 02:33:08 +0300

    Update main.py
    add support tor proxy

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import requests
 from loguru import logger
 from time import sleep
+from random import randint
+
+
+def tor_proxy():
+    proxy_auth = str(randint(1, 0x7fffffff)) + ':' + str(randint(1, 0x7fffffff))
+    return {
+        'http': f'socks5://{proxy_auth}@localhost:9150',
+        'https': f'socks5://{proxy_auth}@localhost:9150'
+    }
 
 
 def main(info: str):
+
+    proxy = None
+    if proxys == 'y' or 'Y':
+        proxy = tor_proxy()
+
     address = info.split(':')[0]
     token = info.split(':')[1]
 
@@ -20,8 +34,10 @@ def main(info: str):
         r = requests.post(
             f'https://discord.com/api/v9/channels/922357353423175680/messages',
             headers=headers,
-            json=payload
+            json=payload,
+            proxies=proxy
         ).json()
+
         if r.get('id') is None:
             logger.error(f'Fail send message for: {address}')
         else:
@@ -32,6 +48,8 @@ def main(info: str):
 if __name__ == '__main__':
     with open('data.txt') as file:
         data = [row.strip() for row in file]
+
+    proxys = input('Tor proxy (y, n): ')
 
     for i in data:
         main(i)
